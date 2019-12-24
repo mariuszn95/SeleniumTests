680bc43a9fbf418864ac562900fe1bfcf0ff00ec
Mariusz Maszkiewicz <Mariusz.Maszkiewicz@unit4.com>
2019-12-24 18:32:11 +0100

    NuGet packages added (ChromeDriver, IEDriver), Configure TextContexts with app.configs

--- Selenium/Driver/Driver.cs
+++ Selenium/Driver/Driver.cs
@@ -1,12 +1,14 @@
 ﻿namespace Selenium.Driver
 {
     using System;
+    using System.Diagnostics;
     using System.Reflection;
 
     using Logger.Logger;
 
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.IE;
 
     public static class Driver
     {
@@ -16,7 +18,22 @@
         {
             LoggerSelenium.LogStart(MethodBase.GetCurrentMethod().Name);
 
-            Instance = new ChromeDriver("C:\\DEV\\SeleniumTests");
+            var browser = TestContexts.TestContexts.TestContexts.Browser;
+
+            switch (browser.ToLower())
+            {
+                case "ie":
+                    Instance = new InternetExplorerDriver(InternetExplorerDriverService.CreateDefaultService(), new InternetExplorerOptions());
+                    break;
+                case "chrome":
+                    Instance = new ChromeDriver(ChromeDriverService.CreateDefaultService(), new ChromeOptions());
+                    break;
+                default:
+                    Instance = new ChromeDriver(ChromeDriverService.CreateDefaultService(), new ChromeOptions());
+                    break;
+            }
+
+            Debug.Assert(Instance != null, nameof(Instance) + " != null");
             Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
             LoggerSelenium.LogEnd(MethodBase.GetCurrentMethod().Name);
--- TestContexts/TestContexts/TestContexts.cs
+++ TestContexts/TestContexts/TestContexts.cs
@@ -13,12 +13,15 @@
 
         private static string baseAddress = ConfigurationManager.AppSettings["BaseAddress"];
 
+        private static string browser = ConfigurationManager.AppSettings["Browser"];
+
         private static string websiteName = ConfigurationManager.AppSettings["WebsiteName"];
 
         public TestContexts()
         {
             ArtifactsPath = TestContext.Parameters.Get("ArtifactsPath");
             BaseAddress = TestContext.Parameters.Get("BaseAddress");
+            Browser = TestContext.Parameters.Get("Browser");
             WebsiteName = TestContext.Parameters.Get("WebsiteName");
         }
 
@@ -66,6 +69,31 @@
                     LoggerTestContexts.LogStart("BaseAddress from CI: " + param);
                     baseAddress = param;
                     LoggerTestContexts.LogEnd("BaseAddress from CI: " + baseAddress);
+                }
+
+                LoggerTestContexts.LogEnd(MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        public static string Browser
+        {
+            get
+            {
+                LoggerTestContexts.LogReturn($"{MethodBase.GetCurrentMethod().Name} ----- {browser}");
+
+                return browser;
+            }
+
+            private set
+            {
+                LoggerTestContexts.LogStart(MethodBase.GetCurrentMethod().Name);
+
+                var param = value;
+                if (param != null)
+                {
+                    LoggerTestContexts.LogStart("Browser from CI: " + param);
+                    browser = param;
+                    LoggerTestContexts.LogEnd("Browser from CI: " + browser);
                 }
 
                 LoggerTestContexts.LogEnd(MethodBase.GetCurrentMethod().Name);
